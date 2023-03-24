bcb6ef6aeb69b3fc7076e1182131319dd005f931
tpalinski <tpalinski@icloud.com>
2023-03-24 18:56:27 +0100

    Add json validation

--- src/words.ts
+++ src/words.ts
@@ -7,7 +7,7 @@ export interface CategoryEntry {
 
 export default class WordGenerator{
 
-	private categoryData: 
+	private categoryData: CategoryEntry[] = []
 
 	public constructor(categories: string[]){
 		
@@ -15,10 +15,27 @@ export default class WordGenerator{
 	
 	private parseCategory(categoryName: string){
 		// Look for categoryName.json file in categories directory
-		fs.readFile('../categories/' + categoryName + '.json', (err: Error, data) => {
+		fs.readFile('../categories/' + categoryName + '.json', (err, data) => {
 			if (err) throw err;
+			//@ts-expect-error
 			let categoryData = JSON.parse(data);
-
+			this.categoryData.push(WordGenerator.validateJson(categoryData))
+			
 		})
+	}
+
+	private static validateJson(json: Object): CategoryEntry {
+		if (json.hasOwnProperty('name') &&
+		    json.hasOwnProperty('content')){
+			let res: CategoryEntry = {
+				//@ts-expect-error
+				name: json.name,
+				//@ts-expect-error
+				content: json.content
+			}
+			return res;
+		} else {
+			throw new TypeError("Malformed category.json file")
+		}
 	}
 }
