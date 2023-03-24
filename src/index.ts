1d323cabf72fca5145823bc70454d4166ab5cec0
tpalinski <tpalinski@icloud.com>
2023-03-25 00:05:56 +0100

    Add socket connection confirmation

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,15 @@ const io = new Server(3001)
 
 let gen = new WordGenerator(['fruits', 'people', 'music']);
 
+const userMap: Map<string, string> = new Map();
+
+
+
 io.on("connection", (socket) => {
-	socket.emit("Connected to the server successfully");
+	socket.emit("connected", "Connected to the server successfully");
+	console.log(`New client connected: ${socket.id}`)
 
-
-	io.on("join-room", (data) => {
+	socket.on("join-room", (data) => {
 		console.log(data)
 	})
 })
