0dc73198f9d27e163874665e33fd1bed734c5b86
tpalinski <tpalinski@icloud.com>
2023-03-25 00:15:57 +0100

    Add room joining

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {Server}from 'socket.io'
 import WordGenerator from './words'
+import { RoomConnectionData } from './types';
 
 const io = new Server(3001)
 
@@ -8,13 +9,15 @@ let gen = new WordGenerator(['fruits', 'people', 'music']);
 const userMap: Map<string, string> = new Map();
 
 
-
 io.on("connection", (socket) => {
 	socket.emit("connected", "Connected to the server successfully");
 	console.log(`New client connected: ${socket.id}`)
 
-	socket.on("join-room", (data) => {
-		console.log(data)
+	socket.on("join-room", (data: RoomConnectionData) => {
+		userMap.set(socket.id, data.nickname)
+		socket.join(data.room);
+		socket.emit("room-confirm", userMap.get(socket.id))
+		console.log(`New client: ${data.nickname} joined room: ${data.room}`)
 	})
 })
 
